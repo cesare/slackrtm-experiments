0ba479bd21a086915e0d965ecbf72ecb0d9fde83
SAWADA Tadashi <cesare@mayverse.jp>
2018-04-29 22:38:02 +0900

    Delete obsolete codes

--- lib/slack_rtm/rtm.ex
+++ lib/slack_rtm/rtm.ex
@@ -8,25 +8,7 @@ defmodule SlackRtm.Rtm do
   def init(token) do
     {:ok, identity, ws} = connect!(token)
     spawn_link(SlackRtm.Listener, :init, [ws])
-    {:ok, {identity, ws, 1}}
-  end
-
-  def send_message(text) do
-    GenServer.cast(__MODULE__, {:send, text})
-  end
-
-  def handle_cast({:send, text}, {identity, websocket, next_message_id}) do
-    id = next_message_id
-    channel = System.get_env("SLACK_CHANNEL_ID")
-    message = %{
-      "id" => id,
-      "type" => "message",
-      "channel" => channel,
-      "text" => text,
-    }
-    json_message = Poison.encode!(message)
-    websocket |> Socket.Web.send!({:text, json_message})
-    {:noreply, {identity, websocket, id + 1}}
+    {:ok, {identity, ws}}
   end
 
   def connect!(token) do
@@ -53,17 +35,5 @@ defmodule SlackRtm.Rtm do
 
   def websocket_connect!(uri) do
     Socket.connect!(uri)
-  end
-
-  def loop(websocket) do
-    case Socket.Web.recv!(websocket) do
-      {:ping, _} ->
-        IO.puts "**** got ping ****"
-        websocket |> Socket.Web.send!({:pong, ""})
-      message ->
-        IO.puts "**** got message: #{inspect message} ****"
-    end
-
-    loop(websocket)
   end
 end
